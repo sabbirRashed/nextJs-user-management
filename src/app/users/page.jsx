365cc5a22f2a19c69b7520780a620aa50c8cde6c
sabbirRashed <srsabbirrahman12@gmail.com>
2026-07-11 04:49:21 +0600

    added create new button as link

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,20 +1,28 @@
+import Link from 'next/link';
 import React from 'react';
 
-const UsersPage = async() => {
+const UsersPage = async () => {
     const fetchUser = await fetch('http://localhost:8000/users');
-    const users =  await fetchUser.json()
+    const users = await fetchUser.json()
     // console.log('userData',users);
     return (
         <div className='w-11/12 mx-auto mt-10'>
             <h2>Users: {users.length}</h2>
             <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
-                {users.map(user => <div 
-                key={user.id}
-                className='border border-purple-500 rounded-2xl p-6 '>
+                {users.map(user => <div
+                    key={user.id}
+                    className='border border-purple-500 rounded-2xl p-6 '>
                     <h2 className='text-2xl'>{user.name}</h2>
                     <p>{user.email}</p>
                 </div>)}
+                <Link href={'/users/new'}
+                className='border border-green-500 rounded-2xl p-6 hover:scale-105 shadow block transition-all duration-300'>
+                    <div >
+                        <h2 className='text-2xl'>Create a new user</h2>
+                    </div>
+                </Link>
             </div>
+
         </div>
     );
 };
